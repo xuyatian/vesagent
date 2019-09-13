abef1ece39f34fc018936ca70ee76993df0464a9
YatianXU <yatian.xu@nokia-sbell.com>
2019-09-13 12:00:25 -0400

    Change CMakeLists.txt

--- src/cmake/xtransportConfig.cmake
+++ src/cmake/xtransportConfig.cmake
@@ -1,6 +1,8 @@
-find_library(XTRANSPORT_LIBRARY xtransport)
-find_path(XTRANSPORT_INCLUDE_DIR xvesagent/xtransport/XTransport.h)
-set(XTRANSPORT_INCLUDE_DIR "${XTRANSPORT_INCLUDE_DIR}/xvesagent/xtransport")
+find_library(XTRANSPORT_LIBRARY xtransport ${PROJECT_BINARY_DIR}/lib/transport)
+IF(NOT DEFINED ${XTRANSPORT_LIBRARY})
+	set(XTRANSPORT_LIBRARY ${PROJECT_BINARY_DIR}/lib/transport/libtransport.so)
+ENDIF()
+find_path(XTRANSPORT_INCLUDE_DIR XTransport.h ${CMAKE_SOURCE_DIR}/lib/transport/include)
 message("xtransport include dir: ${XTRANSPORT_INCLUDE_DIR}")
 message("xtransport library dir: ${XTRANSPORT_LIBRARY}")
 
