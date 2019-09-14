e30645f0dfbfc1efc4a61f02f2d3277592ee95df
YatianXU <yatian.xu@nokia-sbell.com>
2019-09-14 07:23:37 -0400

    CMake

--- src/cmake/xencodeConfig.cmake
+++ src/cmake/xencodeConfig.cmake
@@ -1,6 +1,6 @@
 find_library(XENCODE_LIBRARY xencode ${PROJECT_BINARY_DIR}/lib/encode)
 IF(${XENCODE_LIBRARY} MATCHES "XENCODE_LIBRARY-NOTFOUND")
-	set(XENCODE_LIBRARY ${PROJECT_BINARY_DIR}/lib/transport/libencode.so)
+	set(XENCODE_LIBRARY ${PROJECT_BINARY_DIR}/lib/encode/libxencode.so)
 ENDIF()
 find_path(XENCODE_INCLUDE_DIR XEvent.h ${CMAKE_SOURCE_DIR}/lib/encode/include)
 message("xencode include dir: ${XENCODE_INCLUDE_DIR}")
--- src/cmake/xtransportConfig.cmake
+++ src/cmake/xtransportConfig.cmake
@@ -1,6 +1,6 @@
 find_library(XTRANSPORT_LIBRARY xtransport ${PROJECT_BINARY_DIR}/lib/transport)
 IF(XTRANSPORT_LIBRARY MATCHES "XTRANSPORT_LIBRARY-NOTFOUND")
-	set(XTRANSPORT_LIBRARY ${PROJECT_BINARY_DIR}/lib/transport/libtransport.so)
+	set(XTRANSPORT_LIBRARY ${PROJECT_BINARY_DIR}/lib/transport/libxtransport.so)
 ENDIF()
 find_path(XTRANSPORT_INCLUDE_DIR XTransport.h ${CMAKE_SOURCE_DIR}/lib/transport/include)
 message("xtransport include dir: ${XTRANSPORT_INCLUDE_DIR}")
